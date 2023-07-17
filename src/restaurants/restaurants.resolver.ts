285b20bba9cde13f0c2a014523cf1fc0119cfeb5
dugidugi <duggis92@gmail.com>
2023-07-17 13:23:31 +0900

    feat: argument in Query

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,11 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurants } from './entities/restaurants.entitiy';
 
 @Resolver()
 export class RestaurantsResolver {
-  @Query(() => Restaurants)
-  myRestaurants() {
-    return true;
+  @Query(() => [Restaurants])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurants[] {
+    console.log(veganOnly);
+    return [];
   }
 }
