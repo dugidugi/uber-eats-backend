568f9b9badf9af3695b07a6b203e164c889bcb14
dugidugi <duggis92@gmail.com>
2023-07-27 12:19:39 +0900

    fix: 데코레이터 잘못 적음

--- src/users/dtos/login.dto.ts
+++ src/users/dtos/login.dto.ts
@@ -1,11 +1,11 @@
-import { Field, PickType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType, PickType } from '@nestjs/graphql';
 import { MutationOutputDto } from 'src/common/dtos/response.entity';
 import { User } from '../entities/user.entity';
 
-@Object()
+@InputType()
 export class LoginInputDto extends PickType(User, ['email', 'password']) {}
 
-@Object()
+@ObjectType()
 export class LoginOutputDto extends MutationOutputDto {
   @Field(() => String, { nullable: true })
   token?: string;
