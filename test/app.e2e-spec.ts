f257c595873e355ff0468f9b33b60f72dedb61e3
dugidugi <duggis92@gmail.com>
2023-08-23 09:30:57 +0900

    test: userProfile query e2e 테스트

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,7 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { DataSource } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 const GRAPHQL_ENDPOINT = '/graphql';
 
@@ -14,11 +16,7 @@ jest.mock('got', () => {
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-  const testUser = {
-    email: 'yooduck.h@gmail.com',
-    password: '12345',
-  };
-  let jwtToken: string;
+  let usersRepository: Repository<User>;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -26,6 +24,7 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = module.createNestApplication();
+    usersRepository = module.get(getRepositoryToken(User));
     await app.init();
   });
 
@@ -43,6 +42,12 @@ describe('AppController (e2e)', () => {
     await dataSource.dropDatabase();
     await app.close();
   });
+
+  const testUser = {
+    email: 'yooduck.h@gmail.com',
+    password: '12345',
+  };
+  let jwtToken: string;
 
   describe('createAccount', () => {
     it('should create account', () => {
@@ -152,8 +157,78 @@ describe('AppController (e2e)', () => {
         });
     });
   });
+  describe('userProfile', () => {
+    let userId: number;
+
+    beforeAll(async () => {
+      const users = await usersRepository.find();
+      const [user] = users;
+      userId = user.id;
+    });
+    it('유저 프로필을 성공적으로 가져온다', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .set('X-JWT', jwtToken)
+        .send({
+          query: `
+            query {
+              userProfile(userId:${userId}){
+                ok,
+                error,
+                user {
+                  id
+                }
+              }
+          }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            data: {
+              userProfile: {
+                ok,
+                error,
+                user: { id },
+              },
+            },
+          } = res.body;
+          expect(ok).toEqual(true);
+          expect(error).toBeNull();
+          expect(id).toEqual(userId);
+        });
+    });
+
+    it('유저 프로필을 가져오는데 실패한다', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .set('X-JWT', jwtToken)
+        .send({
+          query: `
+            query {
+              userProfile(userId:${22222}){
+                ok,
+                error,
+                user {
+                  id
+                }
+              }
+          }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            data: {
+              userProfile: { ok, error, user },
+            },
+          } = res.body;
+          console.log(res.body.data);
+          expect(ok).toEqual(false);
+          expect(error).toEqual('User not found');
+          expect(user).toBeNull();
+        });
+    });
+  });
   it.todo('me');
-  it.todo('userProfile');
   it.todo('editProfile');
   it.todo('verifyEmail');
 });
