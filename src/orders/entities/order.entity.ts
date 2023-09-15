17752b2f2628d6387827dc6cfe89a02300f9a504
dugidugi <duggis92@gmail.com>
2023-09-15 11:01:55 +0900

    feat: order entitiy - cutomer, rider, restaurant 칼럼 eager로 세팅

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -38,6 +38,7 @@ export class Order extends CoreEntity {
   @ManyToOne((type) => User, (user) => user.orders, {
     onDelete: 'SET NULL',
     nullable: true,
+    eager: true,
   })
   customer?: User;
 
@@ -48,6 +49,7 @@ export class Order extends CoreEntity {
   @ManyToOne((type) => User, (user) => user.rides, {
     onDelete: 'SET NULL',
     nullable: true,
+    eager: true,
   })
   rider?: User;
 
@@ -58,6 +60,7 @@ export class Order extends CoreEntity {
   @ManyToOne((type) => Restaurant, (restaurant) => restaurant.orders, {
     onDelete: 'SET NULL',
     nullable: true,
+    eager: true,
   })
   restaurant?: Restaurant;
 
