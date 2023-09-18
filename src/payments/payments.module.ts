0666ddfe23598cf32a55dbe93fbd62c74a607fac
dugidugi <duggis92@gmail.com>
2023-09-18 14:15:21 +0900

    fix: paymeny module imorts Restuarant entity

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -3,9 +3,10 @@ import { PaymentResolver } from './payment.resolver';
 import { PaymentService } from './payment.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payment } from './entities/payment.entity';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 
 @Module({
   providers: [PaymentResolver, PaymentService],
-  imports: [TypeOrmModule.forFeature([Payment])],
+  imports: [TypeOrmModule.forFeature([Payment, Restaurant])],
 })
 export class PaymentsModule {}
